ec0cc27a99820ad43e3de3753482c8986701d05e
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-01-21 22:40:37 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,27 @@
+
+
+// first function
+
+// function kilometerToMeter(kilometer){
+//     var meter= kilometer*1000;
+//     return meter;
+// }
+
+
+//  second function
+
+
+function budgetCalculator(watch,phone,laptop){
+        var totalWatchPrice= watch*50;
+        var totalPhonePrice=phone*100;
+        var totalLaptopPrice=laptop*500;
+
+        var total=totalWatchPrice+totalPhonePrice+totalLaptopPrice;
+
+        return total;
+}
+var result=budgetCalculator(1,1,1);
+console.log(result);
+
+// third function
+
