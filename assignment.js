269e1eed66b7b443ce596aec6f11e3569db335bd
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-01-21 23:01:31 +0600

    another commit

--- assignment.js
+++ assignment.js
@@ -11,17 +11,44 @@
 //  second function
 
 
-function budgetCalculator(watch,phone,laptop){
-        var totalWatchPrice= watch*50;
-        var totalPhonePrice=phone*100;
-        var totalLaptopPrice=laptop*500;
+// function budgetCalculator(watch,phone,laptop){
+//         var totalWatchPrice = watch * 50;
+//         var totalPhonePrice = phone * 100;
+//         var totalLaptopPrice = laptop * 500;
 
-        var total=totalWatchPrice+totalPhonePrice+totalLaptopPrice;
+//         var total = totalWatchPrice + totalPhonePrice + totalLaptopPrice;
 
-        return total;
-}
-var result=budgetCalculator(1,1,1);
-console.log(result);
+//         return total;
+// }
+// var result = budgetCalculator(1,1,1);
+// console.log(result);
 
 // third function
 
+function hotelCost(days){
+        var cost=0;
+        if(days <= 10){
+            cost=days*100;
+        }
+        else if(days <= 20){
+            var firstTenDays = 10 * 100;
+            var remaining = days - 10;
+            var extraDays = remaining*80;
+            cost = firstTenDays + extraDays;
+
+        }
+        else{
+           var firstTenDays = 10 * 100;
+           var nextTendays = 10* 80;
+           var remaining= days-20;
+
+           var extraDays= remaining * 50;
+           cost = firstTenDays+nextTendays+extraDays;
+
+        }
+        return cost;
+
+}
+
+var result= hotelCost(21);
+console.log(result);
